a9cfb59f91718afe779788fa7251c97395adcecc
JorgeCharles3418 <157080919+JorgeCharles3418@users.noreply.github.com>
2024-01-28 17:53:19 -0600

    Update PR2_Prueba.m

--- PR2_Prueba.m
+++ PR2_Prueba.m
@@ -3,14 +3,14 @@
 %Aqui se crea la figura donde se va a graficar el x con respecto a t
 %s y t son parametros que retorna la función ode45
 figure(1)
-plot(t,x(:,1));
+plot(t,x(:,2));
 grid on
 hold on
 title("Posición");
 xlabel("Tiempo");
 ylabel("Radianes");
 figure(2)
-plot(t,x(:,3));
+plot(t,x(:,4));
 grid on
 title("Velocidad");
 xlabel("Tiempo");
