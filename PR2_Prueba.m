c39ba401e0c783ea85f8079803695449715c4435
JorgeCharles3418 <157080919+JorgeCharles3418@users.noreply.github.com>
2024-01-28 07:55:45 -0600

    Update PR2_Prueba.m

--- PR2_Prueba.m
+++ PR2_Prueba.m
@@ -1,12 +1,12 @@
-[t,x]=ode45(@PR2,[0 15],[0 0 35 0]);
-%odea45 (función, tiempo de muestreo, valores iniciales)
+[t,x]=ode45(@PR2,[0 15],[0 0 0.610865 0]);
+%odea45 (funciÃ³n, tiempo de muestreo, valores iniciales)
 %Aqui se crea la figura donde se va a graficar el x con respecto a t
-%s y t son parametros que retorna la función ode45
+%s y t son parametros que retorna la funciÃ³n ode45
 figure(1)
 plot(t,x(:,1));
 grid on
 hold on
-title("Posición");
+title("PosiciÃ³n");
 xlabel("Tiempo");
 ylabel("Radianes");
 figure(2)
